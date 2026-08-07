785a79f6646e519fc7355bd50e2b9bfb63d2c4a7
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-07 15:00:55 +0530

    Update and rename __init__.py to config_parser.py

---
+++ TechVJ/utils/config_parser.py
@@ -0,0 +1,23 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from os import environ
+from typing import Dict, Optional
+
+
+class TokenParser:
+    def __init__(self, config_file: Optional[str] = None):
+        self.tokens = {}
+        self.config_file = config_file
+
+    def parse_from_env(self) -> Dict[int, str]:
+        self.tokens = dict(
+            (c + 1, t)
+            for c, (_, t) in enumerate(
+                filter(
+                    lambda n: n[0].startswith("MULTI_TOKEN"), sorted(environ.items())
+                )
+            )
+        )
+        return self.tokens
