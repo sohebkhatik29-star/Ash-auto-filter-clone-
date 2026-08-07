361080593a224408d1aa81409b121348fbfcf2f3
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-07 14:57:32 +0530

    Update __init__.py

--- TechVJ/server/__init__.py
+++ TechVJ/server/__init__.py
@@ -1,1 +1,12 @@
-# TechVJ server init
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+from aiohttp import web
+from .stream_routes import routes
+
+
+async def web_server():
+    web_app = web.Application(client_max_size=30000000)
+    web_app.add_routes(routes)
+    return web_app
