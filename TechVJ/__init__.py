c5636ccd6cac58baffb3a2a0d509d5597fe43fd4
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-07 15:08:09 +0530

    Update __init__.py

--- TechVJ/__init__.py
+++ TechVJ/__init__.py
@@ -1,1 +1,8 @@
-# TechVJ package init
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import time
+
+StartTime = time.time()
+__version__ = 1.1
