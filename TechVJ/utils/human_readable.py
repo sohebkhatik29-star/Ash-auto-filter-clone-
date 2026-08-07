a8ef6da29920368d98df0257522d0ac07ec657fe
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-07 15:03:33 +0530

    Create human_readable.py

---
+++ TechVJ/utils/human_readable.py
@@ -0,0 +1,16 @@
+# Don't Remove Credit @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot @Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+def humanbytes(size):
+    # https://stackoverflow.com/a/49361727/4723940
+    # 2**10 = 1024
+    if not size:
+        return ""
+    power = 2**10
+    n = 0
+    Dic_powerN = {0: ' ', 1: 'Ki', 2: 'Mi', 3: 'Gi', 4: 'Ti'}
+    while size > power:
+        size /= power
+        n += 1
+    return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
