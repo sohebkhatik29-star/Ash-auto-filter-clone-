7a0896cb0f4b706084347414028598a65c160906
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-08 15:00:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "VJ File Store Bot"
+@app.route('/')
+def hello_world():
+    return 'TechVJ'
+
+
+if __name__ == "__main__":
+    app.run()
