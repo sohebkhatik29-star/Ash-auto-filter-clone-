02da5e38c1b216f52c6714d5168c666c86f772e5
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-08 13:41:42 +0530

    Update clone.py

--- plugins/clone.py
+++ plugins/clone.py
@@ -1,1 +1,88 @@
-# plugins file clone
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+import re
+from pymongo import MongoClient
+from Script import script
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors.exceptions.bad_request_400 import AccessTokenExpired, AccessTokenInvalid
+from config import API_ID, API_HASH, DB_URI, DB_NAME, CLONE_MODE
+
+mongo_client = MongoClient(DB_URI)
+mongo_db = mongo_client["cloned_vjbotz"]
+
+@Client.on_message(filters.command("clone") & filters.private)
+async def clone(client, message):
+    if CLONE_MODE == False:
+        return 
+    techvj = await client.ask(message.chat.id, "<b>1) sᴇɴᴅ <code>/newbot</code> ᴛᴏ @BotFather\n2) ɢɪᴠᴇ ᴀ ɴᴀᴍᴇ ꜰᴏʀ ʏᴏᴜʀ ʙᴏᴛ.\n3) ɢɪᴠᴇ ᴀ ᴜɴɪǫᴜᴇ ᴜsᴇʀɴᴀᴍᴇ.\n4) ᴛʜᴇɴ ʏᴏᴜ ᴡɪʟʟ ɢᴇᴛ ᴀ ᴍᴇssᴀɢᴇ ᴡɪᴛʜ ʏᴏᴜʀ ʙᴏᴛ ᴛᴏᴋᴇɴ.\n5) ꜰᴏʀᴡᴀʀᴅ ᴛʜᴀᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴇ.\n\n/cancel - ᴄᴀɴᴄᴇʟ ᴛʜɪs ᴘʀᴏᴄᴇss.</b>")
+    if techvj.text == '/cancel':
+        await techvj.delete()
+        return await message.reply('<b>ᴄᴀɴᴄᴇʟᴇᴅ ᴛʜɪs ᴘʀᴏᴄᴇss 🚫</b>')
+    if techvj.forward_from and techvj.forward_from.id == 93372553:
+        try:
+            bot_token = re.findall(r"\b(\d+:[A-Za-z0-9_-]+)\b", techvj.text)[0]
+        except:
+            return await message.reply('<b>sᴏᴍᴇᴛʜɪɴɢ ᴡᴇɴᴛ ᴡʀᴏɴɢ 😕</b>')
+    else:
+        return await message.reply('<b>ɴᴏᴛ ꜰᴏʀᴡᴀʀᴅᴇᴅ ꜰʀᴏᴍ @BotFather 😑</b>')
+    user_id = message.from_user.id
+    msg = await message.reply_text("**👨‍💻 ᴡᴀɪᴛ ᴀ ᴍɪɴᴜᴛᴇ ɪ ᴀᴍ ᴄʀᴇᴀᴛɪɴɢ ʏᴏᴜʀ ʙᴏᴛ ❣️**")
+    try:
+        vj = Client(
+            f"{bot_token}", API_ID, API_HASH,
+            bot_token=bot_token,
+            plugins={"root": "clone_plugins"}
+        )
+        await vj.start()
+        bot = await vj.get_me()
+        details = {
+            'bot_id': bot.id,
+            'is_bot': True,
+            'user_id': user_id,
+            'name': bot.first_name,
+            'token': bot_token,
+            'username': bot.username
+        }
+        mongo_db.bots.insert_one(details)
+        await msg.edit_text(f"<b>sᴜᴄᴄᴇssғᴜʟʟʏ ᴄʟᴏɴᴇᴅ ʏᴏᴜʀ ʙᴏᴛ: @{bot.username}.</b>")
+    except BaseException as e:
+        await msg.edit_text(f"⚠️ <b>Bot Error:</b>\n\n<code>{e}</code>\n\n**Kindly forward this message to @KingVJ01 to get assistance.**")
+
+@Client.on_message(filters.command("deletecloned") & filters.private)
+async def delete_cloned_bot(client, message):
+    if CLONE_MODE == False:
+        return 
+    try:
+        techvj = await client.ask(message.chat.id, "**Send Me Bot Token To Delete**")
+        bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', techvj.text, re.IGNORECASE)
+        bot_token = bot_token[0] if bot_token else None
+        bot_id = re.findall(r'\d[0-9]{8,10}', techvj.text)
+        cloned_bot = mongo_db.bots.find_one({"token": bot_token})
+        if cloned_bot:
+            mongo_db.bots.delete_one({"token": bot_token})
+            await message.reply_text("**🤖 ᴛʜᴇ ᴄʟᴏɴᴇᴅ ʙᴏᴛ ʜᴀs ʙᴇᴇɴ ʀᴇᴍᴏᴠᴇᴅ ғʀᴏᴍ ᴛʜᴇ ʟɪsᴛ ᴀɴᴅ ɪᴛs ᴅᴇᴛᴀɪʟs ʜᴀᴠᴇ ʙᴇᴇɴ ʀᴇᴍᴏᴠᴇᴅ ғʀᴏᴍ ᴛʜᴇ ᴅᴀᴛᴀʙᴀsᴇ. ☠️**")
+        else:
+            await message.reply_text("**⚠️ ᴛʜᴇ ʙᴏᴛ ᴛᴏᴋᴇɴ ᴘʀᴏᴠɪᴅᴇᴅ ɪs ɴᴏᴛ ɪɴ ᴛʜᴇ ᴄʟᴏɴᴇᴅ ʟɪsᴛ.**")
+    except:
+        await message.reply_text("An error occurred while deleting the cloned bot.")
+
+# Don't Remove Credit Tg - @VJ_Bots
+# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
+# Ask Doubt on telegram @KingVJ01
+
+async def restart_bots():
+    bots = list(mongo_db.bots.find())
+    for bot in bots:
+        bot_token = bot['token']
+        try:
+            vj = Client(
+                f"{bot_token}", API_ID, API_HASH,
+                bot_token=bot_token,
+                plugins={"root": "clone_plugins"},
+            )
+            await vj.start()
+        except:
+            pass
