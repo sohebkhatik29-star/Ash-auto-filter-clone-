7899350c1290d88cc19bba779a9d34789918da6a
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-08-09 19:15:53 +0530

    Remove old VJ branding from web app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'TechVJ'
+    return 'ASH FILE STORE & CLONE MANAGER BOT'
 
 
 if __name__ == "__main__":
