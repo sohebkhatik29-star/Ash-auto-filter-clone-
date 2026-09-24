44c3f8328653ab9ec240f7ab92325d3f4601e11c
sohebkhatik29-star <sohebkhatik29@gmail.com>
2026-09-24 05:11:12 +0000

    Update default configuration with user credentials directly in config.py

--- config.py
+++ config.py
@@ -13,22 +13,22 @@ def is_enabled(value, default):
         return default
 
 # Bot Information
-API_ID = int(environ.get("API_ID", "0"))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "30720676"))
+API_HASH = environ.get("API_HASH", "a078e3476750afbd6db7d6c5e5e658d9")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8892405396:AAEVBCZvMB9VRyMuHP3t8sNt4a2E6DaiByk")
 
 PICS = (environ.get('PICS', '')).split()
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '5566977478').split()]
 BOT_USERNAME = environ.get("BOT_USERNAME", "Ash_files_or_clone_mangar_bot")
 PORT = environ.get("PORT", "8080")
 
 # Clone Info
-CLONE_MODE = is_enabled(environ.get('CLONE_MODE', 'False'), False)
-CLONE_DB_URI = environ.get("CLONE_DB_URI") or environ.get("DB_URI", "")
+CLONE_MODE = is_enabled(environ.get('CLONE_MODE', 'True'), True)
+CLONE_DB_URI = environ.get("CLONE_DB_URI", "mongodb+srv://sohebkhatik8780_db_user:brPx0UDlM81rr91N@cluster0.qempoax.mongodb.net/?appName=Cluster0")
 CDB_NAME = environ.get("CDB_NAME", "ash_clone_db")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://sohebkhatik8780_db_user:brPx0UDlM81rr91N@cluster0.qempoax.mongodb.net/?appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "ash_file_store")
 
 # Auto Delete Information
@@ -37,7 +37,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30"))
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800"))
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "0"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003444819050"))
 SUPPORT_GROUP = environ.get("SUPPORT_GROUP", "ash_movie_j")
 UPDATE_CHANNEL = environ.get("UPDATE_CHANNEL", environ.get("UPDATES_CHANNEL", "MoviesGroupG3"))
 
@@ -56,7 +56,7 @@ BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
 PUBLIC_FILE_STORE = is_enabled(environ.get('PUBLIC_FILE_STORE', "True"), True)
 
 # Verify Info
-VERIFY_MODE = is_enabled(environ.get('VERIFY_MODE', 'False'), False)
+VERIFY_MODE = is_enabled(environ.get('VERIFY_MODE', 'True'), True)
 SHORTLINK_URL = environ.get("SHORTLINK_URL", "")
 SHORTLINK_API = environ.get("SHORTLINK_API", "")
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "")
